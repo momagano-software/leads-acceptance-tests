2cbbd170a2ac01a31427ba00e8718ef8570f39fe
Bakwanyana Thobela <bmthobela@gmail.com>
2022-02-06 22:01:08 +0200

    initial commit

---
+++ src/main/java/za/co/momagano/app/Calculator.java
@@ -0,0 +1,7 @@
+package za.co.momagano.app;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/za/co/momagano/acceptancetests/AcceptanceTestSuite.java
@@ -0,0 +1,9 @@
+package za.co.momagano.acceptancetests;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/", glue="za.co.momagano")
+public class AcceptanceTestSuite {}
